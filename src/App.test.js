331838ba5e7b78d597205dc453d1e03a44937530
NinaKWelch <ninakwelch@gmail.com>
2019-11-18 23:00:17 +0000

    add react router

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+/*
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
+*/
+
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
+
+it('renders without crashing', () => {
+  const { getByText } = render(<App />)
+  expect(getByText('App')).toBeInTheDocument()
+})
